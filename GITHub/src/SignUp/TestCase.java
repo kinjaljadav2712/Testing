a20d5126883bc06b05ebaffa312a9bd8e2ecacda
Kinjal2712 <88541120+Kinjal2712@users.noreply.github.com>
2021-09-18 22:10:50 +0100

    This will be commited by Ki njal

---
+++ GITHub/src/SignUp/TestCase.java
@@ -0,0 +1,13 @@
+package SignUp;
+
+public class TestCase {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		
+		System.out.println("This Tc is about sign up");
+
+	}
+
+}
